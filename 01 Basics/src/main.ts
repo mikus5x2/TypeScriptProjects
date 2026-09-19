1d82726b74c0af86ebf19d59487474a38925bf8f
Michal <mikus@Michals-MacBook-Air.local>
2026-09-19 12:17:16 +0200

    finished module 1 - absolute basics

--- 01 Basics/src/main.ts
+++ 01 Basics/src/main.ts
@@ -1,2 +1,22 @@
-const name: string = "bomba";
-console.log(name)
+console.log("Hello, world!")
+console.log("Hello, world 2!")
+console.log(7 + 5)
+console.log("'hi'")
+// This is a comment!     
+const name = "Ada"
+let age = 18
+age = age + 10
+console.log(name, age)
+console.log(typeof name, typeof age, typeof true)
+
+const subtotal = 80
+const discount = 0.15
+const finalPrice = subtotal * (1 - discount)
+console.log(finalPrice)
+console.log(10 % 2)
+
+const number = 18
+const hasTicket = true;
+console.log(number >= 18 && hasTicket)
+console.log(number < 18 && !hasTicket)
+console.log(number)
